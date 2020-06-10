4a26796538312bc61320eddc34f7e6154e60c2ba
Filipe Duarte <filipe_pb_duarte@hotmail.com>
2020-06-10 18:48:38 -0300

    upando aula1.R

---
+++ aula1.R
@@ -0,0 +1,145 @@
+# Comentários do códigos
+10
+print(10)
+?print
+
+1 + 1
+print(1+1)
+
+10 - 2
+
+print(10 - 2)
+
+
+### Atribuição de variáveis
+# x = 2
+x <- 2 # o comando alt + - é um atalho para a seta de atribuição " <- " 
+x
+print(x)
+
+x = 2 
+x = 3
+x <- 3
+
+# Crie duas variáveis, X (valor) e N (quantidade), recebendo 50 e 3, respectivamente. 
+# Faça também um comentário, para cada variável, detalhando o seu nome.
+
+X <- 50 # X é valor
+N <- 3 # N é quantidade
+
+##### Operações matemáticas
+
+# Os operadores matemáticas no R são:
+# adição: + 
+# subtração: -
+# multiplicação: *
+# divisão: /
+# exponenciação: ^ ou **
+# resto da divisão: %%
+# divisão inteira: %/%
+
+# somar 2 a 2
+2 + 2
+
+# subtraia 50 de 5
+50 - 5
+
+# multiplicação de 10 por 40
+10 * 40
+
+# dividir 50 por 3
+50 / 3
+
+# eleve 2 a 5
+2 ^ 5
+2 ** 5
+
+# resto da divisão de 6 por 4
+6 %% 4
+
+# divisão inteira de 6 por 4
+6 %/% 4
+
+
+y <- 2 ^ 5
+y
+
+# Sua vez de exercitar:
+
+# Adicione 30 à X 
+
+# Subtraia 3 de N
+
+# Eleve 3 ao cubo
+
+# Mostre o resto da divisão de 30 por 7
+
+# mostre o valor da divisão inteira de 9 por 4
+
+# 2 * 5 + 3 ^ 5 
+(2 * 5) + (3^5)
+
+(3 * X) + (N^4)
+
+((2 + X)^2) - ((N/10) + (X^2))^5
+
+options(scipen = 999)
+
+X ** 10
+
+# Sua vez: Calcule o produto de N e X, adicione a divisão de 9 por 10, e multiplique o resultado pela subtração de 5 por 2. 
+
+
+# Tipos de objetos
+
+# O objeto mais comum no é o vetor. Um vetor apresenta o mesmo tipo para os seus valores, que podem ser do tipo:
+
+# números inteiros. ex.: 10, 5, 1
+# números reais. ex.: 10.5
+# números complexos. ex.: 1i
+# valores booleanos. ex.: TRUE ou FALSE
+# caracteres. ex.: "Filipe" 
+meunome <- "Filipe"
+meu_nome <- "Filipe"
+meuNome <- "Filipe"
+1meu_nome # não é possível
+meu_nome1 <- "João"
+meu_nome_1 <- "João"
+
+# Operadores de comparação
+
+# São utilizados nas operações condicionais cujos resultados são valores VERDADEIRO (TRUE) ou FALSO (FALSE). 
+# Utilizamos um operador de comparação para realizar a operação entre dois valores. 
+X > 5
+X < 5
+
+# menor: <
+# maior: >
+# menor ou igual: <=
+# maior ou igual: >=
+# igual: ==
+# diferente != 
+
+x <- 10
+y <- 5
+
+# menor
+x < y
+
+# maior
+x > y
+
+# menor ou igual
+x <= y
+
+# maior ou igual
+x >= y
+
+# igualdade
+x == y
+
+# diferente
+x != y
+
+
+# Final da aula! 
